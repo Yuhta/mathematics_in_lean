456e5052ee7d8f4accea60bf716315fbc260a93f
Jimmy Lu <gongchuo.lu@gmail.com>
2024-01-13 20:53:47 -0500

    C02S04

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,17 +39,56 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply max_le
+    apply le_max_right
+    apply le_max_left
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  apply le_antisymm
+  . apply le_min
+    . calc
+        min (min a b) c ≤ min a b := by apply min_le_left
+        _ ≤ a := by apply min_le_left
+    . apply le_min
+      . calc
+        min (min a b) c ≤ min a b := by apply min_le_left
+        _ ≤ b := by apply min_le_right
+      . apply min_le_right
+  . apply le_min
+    . apply le_min
+      . apply min_le_left
+      . calc
+          min a (min b c) ≤ min b c := by apply min_le_right
+          _ ≤ b := by apply min_le_left
+    . calc
+        min a (min b c) ≤ min b c := by apply min_le_right
+        _ ≤ c := by apply min_le_right
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  . apply add_le_add_right
+    apply min_le_left
+  . apply add_le_add_right
+    apply min_le_right
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  . apply aux
+  have h : min (a + c) (b + c) + -c ≤ min a b := calc
+    min (a + c) (b + c) + -c ≤ min (a + c + -c) (b + c + -c) := by apply aux
+    _ = min a b := by rw [add_neg_cancel_right, add_neg_cancel_right]
+  linarith
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := by
+  have h : |a| ≤ |a - b| + |b| := calc
+    |a| = |a - b + b| := by rw [sub_add_cancel]
+    _ ≤ |a - b| + |b| := abs_add (a - b) b
+  linarith
+
 end
 
 section
@@ -66,7 +105,13 @@ example : x ∣ x ^ 2 := by
    apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add
+  . apply dvd_add
+    . apply dvd_mul_of_dvd_right
+      apply dvd_mul_right
+    . apply dvd_mul_left
+  . apply dvd_mul_of_dvd_right h
+
 end
 
 section
@@ -78,7 +123,12 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply Nat.dvd_antisymm
+  repeat
+    apply Nat.dvd_gcd
+    . apply Nat.gcd_dvd_right
+    . apply Nat.gcd_dvd_left
+
 end
 
 
